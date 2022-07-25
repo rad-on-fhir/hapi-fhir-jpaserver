82e90a66d910abdfd5137b4697219058a9820e1b
Alexander Drexler <adr@sourceblock.de>
2022-07-25 18:19:34 +0200

    Added Event Creation Date to Mqtt-Payload

--- src/main/java/ca/uhn/fhir/jpa/starter/mqtt/MqttEvent.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/mqtt/MqttEvent.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hl7.fhir.instance.model.api.IIdType;
 
+import java.time.ZonedDateTime;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class MqttEvent {
 
@@ -19,6 +21,8 @@ public class MqttEvent {
 	public final String id;
 	@JsonProperty("version")
 	public final String version;
+	@JsonProperty("createdAt")
+	public final ZonedDateTime createdAt;
 
 	public MqttEvent(BaseResourceMessage.OperationTypeEnum update, IIdType idElement, String fullUrl) {
 		this.event = update.name();
@@ -26,14 +30,16 @@ public class MqttEvent {
 		this.resourceType = idElement.getResourceType();
 		this.id = idElement.getIdPart();
 		this.version = idElement.getVersionIdPart();
+		this.createdAt = ZonedDateTime.now();
 	}
 
 	@JsonCreator
-	public MqttEvent(String event, String fullUrl, String resourceType, String id, String version) {
+	public MqttEvent(String event, String fullUrl, String resourceType, String id, String version, ZonedDateTime createdAt) {
 		this.event = event;
 		this.fullUrl = fullUrl;
 		this.resourceType = resourceType;
 		this.id = id;
 		this.version = version;
+		this.createdAt = createdAt;
 	}
 }
